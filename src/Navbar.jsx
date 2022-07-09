786680e682a2ed8f82c825cf35eb6cae702443eb
Glitch (tungsten-slender-ellipse) <none>
2022-07-09 09:28:01 +0000

    🍢💗 Checkpoint ./src/pages/Home.jsx:663034/1433 ./public/index.html:663034/603 ./src/Navbar.jsx:663034/1170

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,19 +3,55 @@ import { BrowserRouter, Route, Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-mdb-toggle="collapse"
+      data-mdb-target="#navbarNav"
+      aria-controls="navbarNav"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <i class="fas fa-bars"></i>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
-        <li>
-          <Link to="/blogs">Blogs</Link>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
         </li>
-        <li>
-          <Link to="/contact">Contact</Link>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled"
+            >Disabled</a
+          >
         </li>
       </ul>
-    </nav>
+    </div>
+  </div>
+</nav>
+    
+    
+    // <nav>
+    //   <ul>
+    //     <li>
+    //       <Link to="/">Home</Link>
+    //     </li>
+    //     <li>
+    //       <Link to="/blogs">Blogs</Link>
+    //     </li>
+    //     <li>
+    //       <Link to="/contact">Contact</Link>
+    //     </li>
+    //   </ul>
+    // </nav>
   );
 }
 
