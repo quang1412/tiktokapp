e33942184bbe2a41276667027bbcde471120a082
Glitch (tungsten-slender-ellipse) <none>
2022-07-10 06:30:10 +0000

    ❄️🐆 Checkpoint ./src/pages/Alertbox.jsx:663034/17652 ./src/pages/AlertboxOptsLayout.jsx:663034/1988

--- src/pages/AlertboxOptsLayout.jsx
+++ src/pages/AlertboxOptsLayout.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
-export default function alertboxOptsLayout(options){
+export default function alertboxOptsLayout(data){
+  const options = data.opts;
   return (
     <div class="card">
       <div class="card-header">
@@ -26,49 +27,49 @@ export default function alertboxOptsLayout(options){
         </ul>
       </div>
       <div class="card-body text-start overflow-auto">
-          <div class="tab-content" id="pills-tabContent">
-            <div class="tab-pane fade show active" id="pills-general" role="tabpanel" aria-labelledby="general">
-              <form name="general">
-                <div class="row mb-3">
-                  <div class="col-4">
-                    <label class="form-label mb-0">Alert delay</label>
+        <div class="tab-content" id="pills-tabContent">
+          <div class="tab-pane fade show active" id="pills-general" role="tabpanel" aria-labelledby="general">
+            <form name="general">
+              <div class="row mb-3">
+                <div class="col-4">
+                  <label class="form-label mb-0">Alert delay</label>
+                </div>
+                <div class="col-8">
+                  <input name="alert_delay" type="range" class="form-range" min="0" max="30" step="1" value={options.general.alert_delay} data-before="1" data-before-subfix="s"/>
+                </div>
+              </div>
+              <div class="row mb-3">
+                <div class="col-4">
+                  <label class="form-label mb-0">Parry alert</label>
+                </div>
+                <div class="col-8">
+                  <div class="form-check d-inline-block">
+                    <input class="form-check-input" type="radio" value="true" name="alert_parries" onchange="$('.parry_alert_delay').show()"/>
+                    <label class="form-check-label">
+                      On
+                    </label>
                   </div>
-                  <div class="col-8">
-                    <input name="alert_delay" type="range" class="form-range" min="0" max="30" step="1" value={options.general.alert_delay} data-before="1" data-before-subfix="s"/>
+                  <div class="form-check d-inline-block ms-3">
+                    <input class="form-check-input" type="radio" value="false" name="alert_parries" onchange="$('.parry_alert_delay').hide()" checked/>
+                    <label class="form-check-label">
+                      Off
+                    </label>
                   </div>
                 </div>
-                <div class="row mb-3">
-                  <div class="col-4">
-                    <label class="form-label mb-0">Parry alert</label>
-                  </div>
-                  <div class="col-8">
-                    <div class="form-check d-inline-block">
-                      <input class="form-check-input" type="radio" value="true" name="alert_parries" onchange="$('.parry_alert_delay').show()"/>
-                      <label class="form-check-label">
-                        On
-                      </label>
-                    </div>
-                    <div class="form-check d-inline-block ms-3">
-                      <input class="form-check-input" type="radio" value="false" name="alert_parries" onchange="$('.parry_alert_delay').hide()" checked/>
-                      <label class="form-check-label">
-                        Off
-                      </label>
-                    </div>
-                  </div>
+              </div>
+              <div class="row mb-3 parry_alert_delay" style="display:none;">
+                <div class="col-4">
+                  <label class="form-label mb-0">Parry delay</label>
                 </div>
-                <div class="row mb-3 parry_alert_delay" style="display:none;">
-                  <div class="col-4">
-                    <label class="form-label mb-0">Parry delay</label>
-                  </div>
-                  <div class="col-8">
-                    <input name="parry_alert_delay" type="range" class="form-range" min="1" max="20" step="1" value="4" data-before="1" data-before-subfix="s"/>
-                  </div>
+                <div class="col-8">
+                  <input name="parry_alert_delay" type="range" class="form-range" min="1" max="20" step="1" value="4" data-before="1" data-before-subfix="s"/>
                 </div>
-              </form>
-            </div>
-            {options.map((o, i) => (
-              <br/>
-            ))}
+              </div>
+            </form>
+          </div>
+          {options.map((o, i) => (
+            <br/>
+          ))}
         </div>
       </div>
     </div>
