1e840b3e1bc2cbf80c05ec707d9916b55aa0d709
Glitch (tungsten-slender-ellipse) <none>
2022-07-10 18:02:10 +0000

    😻🚞 Checkpoint ./src/pages/Alertbox.jsx:663034/1270

--- src/pages/Alertbox.jsx
+++ src/pages/Alertbox.jsx
@@ -10,11 +10,28 @@ const Alertbox = () => {
   const [options, setOptions] = useState(JSON.parse(localStorage.alertboxOpts || `{ "share": { "alert_duration": 10, "message_template": "", "alert_animation_out": "backOutDown", "image_url": "https://isetup.vn/tiktok/assets/gif/jumpy-t-rex.gif", "sound_volume": 100, "alert_animation_in": "backInDown", "layout": "banner", "alert_text_delay": 0, "font_weight": 800, "text_color": "#ffffff", "text_highlight_color": "#32c3a6", "text_animation": "wiggle", "font_size": 64, "active": true, "sound_url": "https://isetup.vn/tiktok/assets/sound/new-message-4.ogg" }, "gift": { "text_highlight_color": "#32c3a6", "active": true, "text_animation": "wiggle", "alert_animation_out": "backOutDown", "layout": "banner", "text_color": "#ffffff", "image_url": "https://isetup.vn/tiktok/assets/gif/jumpy-t-rex.gif", "sound_url": "https://isetup.vn/tiktok/assets/sound/new-message-4.ogg", "alert_animation_in": "backInDown", "alert_duration": 10, "sound_volume": 50, "message_template": "", "font_size": 64, "font_weight": 800, "alert_min_amount": 0, "alert_text_delay": 0 }, "like": { "text_color": "#ffffff", "image_url": "https://isetup.vn/tiktok/assets/gif/Explosion.gif", "sound_volume": 50, "sound_url": "https://isetup.vn/tiktok/assets/sound/new-message-4.ogg", "font_weight": 800, "alert_animation_out": "backOutDown", "alert_text_delay": 0, "text_highlight_color": "#32c3a6", "alert_animation_in": "backInDown", "text_animation": "wiggle", "layout": "banner", "alert_duration": 10, "font_size": 64, "message_template": "", "active": true }, "general": { "layout": "banner", "alert_parries": true, "parry_alert_delay": 3, "approved_manually": false, "censor_timeout": 0, "background_color": "#80ffac", "alert_delay": 3, "censor_recent_events": true }, "comment": {}, "follow": { "alert_duration": 10, "alert_animation_in": "backInDown", "message_template": "", "alert_animation_out": "backOutDown", "alert_text_delay": 0, "font_size": 64, "image_url": "https://isetup.vn/tiktok/assets/gif/jumpy-t-rex.gif", "text_highlight_color": "#32c3a6", "text_color": "#ffffff", "layout": "banner", "sound_volume": 50, "text_animation": "wiggle", "font_weight": 800, "sound_url": "https://isetup.vn/tiktok/assets/sound/new-message-4.ogg", "active": true } }`));
   
   const socketConnect = () => {
+    changeLog([...log, 'server connecting']);
     let socket = io("https://tiktoktool.glitch.me",  {query: `id=dongnguyenchat`});
     socket.on('connect', () => { 
-      changeLog([...log, 'connected'])
-      console.log('connected')
     })
+    
+    var connecting = setInterval(() => {
+      changeLog(...log, '.');
+    },1000);
+    
+    socket.on('ttConnectRetry', i => {
+      changeLog(...log, `trying to connect (${i}/10)`);
+    });
+    
+    socket.on('ttRoomInfo', data => {
+      console.log(data);
+      changeLog(...log, 'server connected');
+      clearInterval(connecting);
+    });
+    
+    socket.on('ttConnectFail', () => {
+      changeLog(...log, 'server connect fail');
+    });
   }
   
   const handleOptions = newOptions => {
@@ -25,7 +42,7 @@ const Alertbox = () => {
   
   return (
     <div className="App">
-      <div className="layer" id="log" style={{"display": (currentLayout === "log" ? "block" : "none")}}>
+      <div className="layer text-start" id="log" style={{"display": (currentLayout === "log" ? "block" : "none")}}>
         {log.map(text => (<p>{text}</p>))}
       </div>
       <div className="layer" id="play" style={{"display":(currentLayout === "play" ? "block" : "none")}}></div>
