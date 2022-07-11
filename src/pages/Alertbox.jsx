1dc7d2c2d9c72d714412fefeb2cd74b77e5b3037
Glitch (tungsten-slender-ellipse) <none>
2022-07-11 02:34:57 +0000

    ☂️🍗 Checkpoint ./src/pages/Alertbox.jsx:663034/1041

--- src/pages/Alertbox.jsx
+++ src/pages/Alertbox.jsx
@@ -6,8 +6,7 @@ import { io } from "socket.io-client";
 
 const Alertbox = () => {
   const isLoading = useRef(false);
-  const layer = useRef('log')
-  const [layer, setLayer] = useState('log')
+  const [layer, setLayer] = useState("log")
   const [log, changeLog] = useState(["log:"]); 
   const [options, setOptions] = useState(JSON.parse(localStorage.alertboxOpts || `{ "share": { "alert_duration": 10, "message_template": "", "alert_animation_out": "backOutDown", "image_url": "https://isetup.vn/tiktok/assets/gif/jumpy-t-rex.gif", "sound_volume": 100, "alert_animation_in": "backInDown", "layout": "banner", "alert_text_delay": 0, "font_weight": 800, "text_color": "#ffffff", "text_highlight_color": "#32c3a6", "text_animation": "wiggle", "font_size": 64, "active": true, "sound_url": "https://isetup.vn/tiktok/assets/sound/new-message-4.ogg" }, "gift": { "text_highlight_color": "#32c3a6", "active": true, "text_animation": "wiggle", "alert_animation_out": "backOutDown", "layout": "banner", "text_color": "#ffffff", "image_url": "https://isetup.vn/tiktok/assets/gif/jumpy-t-rex.gif", "sound_url": "https://isetup.vn/tiktok/assets/sound/new-message-4.ogg", "alert_animation_in": "backInDown", "alert_duration": 10, "sound_volume": 50, "message_template": "", "font_size": 64, "font_weight": 800, "alert_min_amount": 0, "alert_text_delay": 0 }, "like": { "text_color": "#ffffff", "image_url": "https://isetup.vn/tiktok/assets/gif/Explosion.gif", "sound_volume": 50, "sound_url": "https://isetup.vn/tiktok/assets/sound/new-message-4.ogg", "font_weight": 800, "alert_animation_out": "backOutDown", "alert_text_delay": 0, "text_highlight_color": "#32c3a6", "alert_animation_in": "backInDown", "text_animation": "wiggle", "layout": "banner", "alert_duration": 10, "font_size": 64, "message_template": "", "active": true }, "general": { "layout": "banner", "alert_parries": true, "parry_alert_delay": 3, "approved_manually": false, "censor_timeout": 0, "background_color": "#80ffac", "alert_delay": 3, "censor_recent_events": true }, "comment": {}, "follow": { "alert_duration": 10, "alert_animation_in": "backInDown", "message_template": "", "alert_animation_out": "backOutDown", "alert_text_delay": 0, "font_size": 64, "image_url": "https://isetup.vn/tiktok/assets/gif/jumpy-t-rex.gif", "text_highlight_color": "#32c3a6", "text_color": "#ffffff", "layout": "banner", "sound_volume": 50, "text_animation": "wiggle", "font_weight": 800, "sound_url": "https://isetup.vn/tiktok/assets/sound/new-message-4.ogg", "active": true } }`));
   
@@ -28,7 +27,7 @@ const Alertbox = () => {
       isLoading.current = false;
       console.log(data);
       changeLog(prevLog => [...prevLog, 'tiktok connected']);
-      layer.current = "play";
+      setTimeout(() => {setLayer("setting")}, 3000)
     });
     
     socket.on('ttConnectFail', () => {
@@ -68,15 +67,20 @@ const Alertbox = () => {
     }
   }, [isLoading.current])
   
-  // socketConnect();
+  
+  const LogView = () => {
+    return (
+      log.map((text, i) => (<p key={i} className="mb-0">{text}</p>))
+    )
+  }
   
   return (
     <div className="App">
-      <div className="layer text-start" id="log" style={{"display": (layer.current === "log" ? "block" : "none")}}>
-        {log.map((text, i) => (<p key={i} className="mb-0">{text}</p>))}
+      <div className="layer text-start" id="log" style={{"display": (layer === "log" ? "block" : "none")}}>
+        <LogView />
       </div>
-      <div className="layer" id="play" style={{"display":(layer.current === "play" ? "block" : "none")}}></div>
-      <div className="layer" id="setting" style={{"display":(layer.current === "setting" ? "block" : "none")}}>
+      <div className="layer" id="play" style={{"display":(layer === "play" ? "block" : "none")}}></div>
+      <div className="layer" id="setting" style={{"display":(layer === "setting" ? "block" : "none")}}>
         <AlertboxOpts opts={options} onChangeOptions={handleOptions}/>
       </div>
     </div>
