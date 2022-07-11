9fed912124313c8f238af33eaa3e7362e43631a3
Glitch (tungsten-slender-ellipse) <none>
2022-07-11 02:23:31 +0000

    🍟👡 Checkpoint ./src/pages/Alertbox.jsx:663034/679

--- src/pages/Alertbox.jsx
+++ src/pages/Alertbox.jsx
@@ -5,16 +5,17 @@ import '../style/TextAnimations.css';
 import { io } from "socket.io-client";
 
 const Alertbox = () => {
-  const [log, changeLog] = useState(["log:"]); 
   const isLoading = useRef(false);
-  const layout = useRef('log')
+  const layer = useRef('log')
+  const [layer, setLayer] = useState('log')
+  const [log, changeLog] = useState(["log:"]); 
   const [options, setOptions] = useState(JSON.parse(localStorage.alertboxOpts || `{ "share": { "alert_duration": 10, "message_template": "", "alert_animation_out": "backOutDown", "image_url": "https://isetup.vn/tiktok/assets/gif/jumpy-t-rex.gif", "sound_volume": 100, "alert_animation_in": "backInDown", "layout": "banner", "alert_text_delay": 0, "font_weight": 800, "text_color": "#ffffff", "text_highlight_color": "#32c3a6", "text_animation": "wiggle", "font_size": 64, "active": true, "sound_url": "https://isetup.vn/tiktok/assets/sound/new-message-4.ogg" }, "gift": { "text_highlight_color": "#32c3a6", "active": true, "text_animation": "wiggle", "alert_animation_out": "backOutDown", "layout": "banner", "text_color": "#ffffff", "image_url": "https://isetup.vn/tiktok/assets/gif/jumpy-t-rex.gif", "sound_url": "https://isetup.vn/tiktok/assets/sound/new-message-4.ogg", "alert_animation_in": "backInDown", "alert_duration": 10, "sound_volume": 50, "message_template": "", "font_size": 64, "font_weight": 800, "alert_min_amount": 0, "alert_text_delay": 0 }, "like": { "text_color": "#ffffff", "image_url": "https://isetup.vn/tiktok/assets/gif/Explosion.gif", "sound_volume": 50, "sound_url": "https://isetup.vn/tiktok/assets/sound/new-message-4.ogg", "font_weight": 800, "alert_animation_out": "backOutDown", "alert_text_delay": 0, "text_highlight_color": "#32c3a6", "alert_animation_in": "backInDown", "text_animation": "wiggle", "layout": "banner", "alert_duration": 10, "font_size": 64, "message_template": "", "active": true }, "general": { "layout": "banner", "alert_parries": true, "parry_alert_delay": 3, "approved_manually": false, "censor_timeout": 0, "background_color": "#80ffac", "alert_delay": 3, "censor_recent_events": true }, "comment": {}, "follow": { "alert_duration": 10, "alert_animation_in": "backInDown", "message_template": "", "alert_animation_out": "backOutDown", "alert_text_delay": 0, "font_size": 64, "image_url": "https://isetup.vn/tiktok/assets/gif/jumpy-t-rex.gif", "text_highlight_color": "#32c3a6", "text_color": "#ffffff", "layout": "banner", "sound_volume": 50, "text_animation": "wiggle", "font_weight": 800, "sound_url": "https://isetup.vn/tiktok/assets/sound/new-message-4.ogg", "active": true } }`));
   
   const socketConnect = (id) => {
     isLoading.current = true;
     
     changeLog(prevLog => [...prevLog, 'server connecting...']); 
-    let socket = io("https://tiktoktools.glitch.me/obs",  {query: `id=dongnguyenchat`});
+    let socket = io("https://tiktoktools.glitch.me/obs",  {query: `id=${id}`});
     socket.on('connect', () => {  
       changeLog(prevLog => [...prevLog, ...['server connected', 'tiktok connecting...']]);
     });
@@ -24,15 +25,16 @@ const Alertbox = () => {
     });
     
     socket.on('ttRoomInfo', data => {
-      console.log('server connected', data);
-      changeLog(prevLog => [...prevLog, 'server connected']);
       isLoading.current = false;
+      console.log(data);
+      changeLog(prevLog => [...prevLog, 'tiktok connected']);
+      layer.current = "play";
     });
     
     socket.on('ttConnectFail', () => {
-      console.log('server connect fail');
-      changeLog(prevLog => [...prevLog, 'server connect fail']);
       isLoading.current = false;
+      console.log('server connect fail');
+      changeLog(prevLog => [...prevLog, 'tiktok connect fail']);
     });
   }
   
@@ -48,7 +50,7 @@ const Alertbox = () => {
       socketConnect(id)
     }
     else{
-      changeLog(prevLog => [...prevLog, 'URL invalid']);
+      changeLog(prevLog => [...prevLog, `URL invalid, please enter ULR like this: ${window.location.origin+window.location.pathname}?id={tiktok_id}`]);
     }
   }, []);
   
@@ -70,11 +72,11 @@ const Alertbox = () => {
   
   return (
     <div className="App">
-      <div className="layer text-start" id="log" style={{"display": (layout.current === "log" ? "block" : "none")}}>
+      <div className="layer text-start" id="log" style={{"display": (layer.current === "log" ? "block" : "none")}}>
         {log.map((text, i) => (<p key={i} className="mb-0">{text}</p>))}
       </div>
-      <div className="layer" id="play" style={{"display":(layout.current === "play" ? "block" : "none")}}></div>
-      <div className="layer" id="setting" style={{"display":(layout.current === "setting" ? "block" : "none")}}>
+      <div className="layer" id="play" style={{"display":(layer.current === "play" ? "block" : "none")}}></div>
+      <div className="layer" id="setting" style={{"display":(layer.current === "setting" ? "block" : "none")}}>
         <AlertboxOpts opts={options} onChangeOptions={handleOptions}/>
       </div>
     </div>
