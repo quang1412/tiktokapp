6466c69983aee2104dc9bb6f33c8cb43f049f13f
Glitch (tungsten-slender-ellipse) <none>
2022-07-12 10:46:32 +0000

    🏰👐 Checkpoint ./src/pages/Alertbox/index.jsx:663034/1157

--- src/pages/Alertbox/index.jsx
+++ src/pages/Alertbox/index.jsx
@@ -9,7 +9,6 @@ const Alertbox = () => {
   
   const isLoading = useRef(false);
   const isOutro = useRef(false);
-  const autoPlaySound = useRef(false);
  
   const [options, setOptions] = useState(JSON.parse(localStorage.alertboxOpts || `{ "share": { "alert_duration": 10, "message_template": "", "alert_animation_out": "backOutDown", "image_url": "https://isetup.vn/tiktok/assets/gif/jumpy-t-rex.gif", "sound_volume": 100, "alert_animation_in": "backInDown", "layout": "banner", "alert_text_delay": 0, "font_weight": 800, "text_color": "#ffffff", "text_highlight_color": "#32c3a6", "text_animation": "wiggle", "font_size": 64, "active": true, "sound_url": "https://isetup.vn/tiktok/assets/sound/new-message-4.ogg" }, "gift": { "text_highlight_color": "#32c3a6", "active": true, "text_animation": "wiggle", "alert_animation_out": "backOutDown", "layout": "banner", "text_color": "#ffffff", "image_url": "https://isetup.vn/tiktok/assets/gif/jumpy-t-rex.gif", "sound_url": "https://isetup.vn/tiktok/assets/sound/new-message-4.ogg", "alert_animation_in": "backInDown", "alert_duration": 10, "sound_volume": 50, "message_template": "", "font_size": 64, "font_weight": 800, "alert_min_amount": 0, "alert_text_delay": 0 }, "like": { "text_color": "#ffffff", "image_url": "https://isetup.vn/tiktok/assets/gif/Explosion.gif", "sound_volume": 50, "sound_url": "https://isetup.vn/tiktok/assets/sound/new-message-4.ogg", "font_weight": 800, "alert_animation_out": "backOutDown", "alert_text_delay": 0, "text_highlight_color": "#32c3a6", "alert_animation_in": "backInDown", "text_animation": "wiggle", "layout": "banner", "alert_duration": 10, "font_size": 64, "message_template": "", "active": true }, "general": { "layout": "banner", "alert_parries": true, "parry_alert_delay": 3, "approved_manually": false, "censor_timeout": 0, "background_color": "#80ffac", "alert_delay": 3, "censor_recent_events": true }, "comment": {}, "follow": { "alert_duration": 10, "alert_animation_in": "backInDown", "message_template": "", "alert_animation_out": "backOutDown", "alert_text_delay": 0, "font_size": 64, "image_url": "https://isetup.vn/tiktok/assets/gif/jumpy-t-rex.gif", "text_highlight_color": "#32c3a6", "text_color": "#ffffff", "layout": "banner", "sound_volume": 50, "text_animation": "wiggle", "font_weight": 800, "sound_url": "https://isetup.vn/tiktok/assets/sound/new-message-4.ogg", "active": true } }`));
   const [layer, setLayer] = useState("log");
@@ -103,13 +102,14 @@ const Alertbox = () => {
   }
   
   const playSound = (url = "https://isetup.vn/tiktok/assets/sound/new-message-4.ogg", vol = 50) => {
-    if(autoPlaySound.current){
+    try{
       audio.pause();
       audio.currentTime = 0;
       audio.src = url;
       audio.volume = vol/100;
       audio.play();
     }
+    catch{}
   } 
   
   const delAllType = type => {
@@ -160,9 +160,6 @@ const Alertbox = () => {
     if(!id){
       setLayer("askId");
       return;
-    }
-    if(audio.play()){
-      autoPlaySound.current = true;
     }
     setTiktokId(id);
     socketConnect(id)
