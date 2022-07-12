9e163eea3788c50df6a8b8b9418fa782d3261e8a
Glitch (tungsten-slender-ellipse) <none>
2022-07-12 01:06:06 +0000

    🐮☄️ Checkpoint ./src/pages/Alertbox/index.jsx:663034/1687

--- src/pages/Alertbox/index.jsx
+++ src/pages/Alertbox/index.jsx
@@ -12,6 +12,7 @@ const Alertbox = () => {
  
   const [options, setOptions] = useState(JSON.parse(localStorage.alertboxOpts || `{ "share": { "alert_duration": 10, "message_template": "", "alert_animation_out": "backOutDown", "image_url": "https://isetup.vn/tiktok/assets/gif/jumpy-t-rex.gif", "sound_volume": 100, "alert_animation_in": "backInDown", "layout": "banner", "alert_text_delay": 0, "font_weight": 800, "text_color": "#ffffff", "text_highlight_color": "#32c3a6", "text_animation": "wiggle", "font_size": 64, "active": true, "sound_url": "https://isetup.vn/tiktok/assets/sound/new-message-4.ogg" }, "gift": { "text_highlight_color": "#32c3a6", "active": true, "text_animation": "wiggle", "alert_animation_out": "backOutDown", "layout": "banner", "text_color": "#ffffff", "image_url": "https://isetup.vn/tiktok/assets/gif/jumpy-t-rex.gif", "sound_url": "https://isetup.vn/tiktok/assets/sound/new-message-4.ogg", "alert_animation_in": "backInDown", "alert_duration": 10, "sound_volume": 50, "message_template": "", "font_size": 64, "font_weight": 800, "alert_min_amount": 0, "alert_text_delay": 0 }, "like": { "text_color": "#ffffff", "image_url": "https://isetup.vn/tiktok/assets/gif/Explosion.gif", "sound_volume": 50, "sound_url": "https://isetup.vn/tiktok/assets/sound/new-message-4.ogg", "font_weight": 800, "alert_animation_out": "backOutDown", "alert_text_delay": 0, "text_highlight_color": "#32c3a6", "alert_animation_in": "backInDown", "text_animation": "wiggle", "layout": "banner", "alert_duration": 10, "font_size": 64, "message_template": "", "active": true }, "general": { "layout": "banner", "alert_parries": true, "parry_alert_delay": 3, "approved_manually": false, "censor_timeout": 0, "background_color": "#80ffac", "alert_delay": 3, "censor_recent_events": true }, "comment": {}, "follow": { "alert_duration": 10, "alert_animation_in": "backInDown", "message_template": "", "alert_animation_out": "backOutDown", "alert_text_delay": 0, "font_size": 64, "image_url": "https://isetup.vn/tiktok/assets/gif/jumpy-t-rex.gif", "text_highlight_color": "#32c3a6", "text_color": "#ffffff", "layout": "banner", "sound_volume": 50, "text_animation": "wiggle", "font_weight": 800, "sound_url": "https://isetup.vn/tiktok/assets/sound/new-message-4.ogg", "active": true } }`));
   const [layer, setLayer] = useState("log");
+  const [tiktokId, setTiktokId] = useState("");
   const [log, changeLog] = useState(["log:"]);
   const [eventQueue, setEventQueue] = useState([]);
   const [mainEvent, setMainEvent] = useState({"type": "like", "data" : {}});
@@ -163,10 +164,12 @@ const Alertbox = () => {
   
   useEffect(function(){
     let id = new URLSearchParams(window.location.search).get('id');
+    // setTiktokId(new URLSearchParams(window.location.search).get('id'))
     if(!id){
       changeLog(prevLog => [...prevLog, `URL invalid, please enter ULR like this: ${window.location.origin+window.location.pathname}?id={tiktok_id}`]);
       return;
     }
+    setTiktokId(id)
     socketConnect(id)
     .then(socket => {
       listenSocket(socket); 
@@ -220,6 +223,14 @@ const Alertbox = () => {
     <div className="Alertbox">
       <div className="layer text-start" id="log" style={{"display": (layer === "log" ? "block" : "none")}}>
         {log.map((text, i) => (<p key={i} className="mb-0">{text}</p>))}
+      </div>
+      <div className="layer" id="askId">
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" placeholder="Enter Tiktok id" aria-label="Enter Tiktok id"/>
+          <button onClick={e=>{}} class="btn btn-outline-primary" type="button" data-mdb-ripple-color="dark">
+            Kết nối
+          </button>
+        </div>
       </div>
       <div className="layer" id="play" style={{"display":(layer === "play" ? "block" : "none")}}>
         <button onClick={e => {setLayer("setting")}} className="btn btn-sm btn-light position-absolute top-0 end-0 text-secondary border lh-1 p-2 m-2" style={{"zIndex":"1"}}><i className="fas fa-cog"></i></button>
