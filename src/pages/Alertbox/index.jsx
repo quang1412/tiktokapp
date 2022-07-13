6a84cf5d70daafdcddf4b8024b579b32985a0585
Glitch (tungsten-slender-ellipse) <none>
2022-07-13 07:56:40 +0000

    🐣🌄 Checkpoint ./src/pages/Alertbox/index.jsx:663034/2432

--- src/pages/Alertbox/index.jsx
+++ src/pages/Alertbox/index.jsx
@@ -14,6 +14,7 @@ const Alertbox = () => {
   const isLoading = useRef(false);
   const isOutro = useRef(false);
   const canAutoPlay = useRef(false);
+  const isDelay = useRef(false);
  
   const [options, setOptions] = useState(JSON.parse(localStorage.alertboxOpts || `{ "share": { "alert_duration": 10, "message_template": "", "alert_animation_out": "backOutDown", "image_url": "https://isetup.vn/tiktok/assets/gif/jumpy-t-rex.gif", "sound_volume": 100, "alert_animation_in": "backInDown", "layout": "banner", "alert_text_delay": 0, "font_weight": 800, "text_color": "#ffffff", "text_highlight_color": "#32c3a6", "text_animation": "wiggle", "font_size": 64, "active": true, "sound_url": "https://isetup.vn/tiktok/assets/sound/new-message-4.ogg" }, "gift": { "text_highlight_color": "#32c3a6", "active": true, "text_animation": "wiggle", "alert_animation_out": "backOutDown", "layout": "banner", "text_color": "#ffffff", "image_url": "https://isetup.vn/tiktok/assets/gif/jumpy-t-rex.gif", "sound_url": "https://isetup.vn/tiktok/assets/sound/new-message-4.ogg", "alert_animation_in": "backInDown", "alert_duration": 10, "sound_volume": 50, "message_template": "", "font_size": 64, "font_weight": 800, "alert_min_amount": 0, "alert_text_delay": 0 }, "like": { "text_color": "#ffffff", "image_url": "https://isetup.vn/tiktok/assets/gif/Explosion.gif", "sound_volume": 50, "sound_url": "https://isetup.vn/tiktok/assets/sound/new-message-4.ogg", "font_weight": 800, "alert_animation_out": "backOutDown", "alert_text_delay": 0, "text_highlight_color": "#32c3a6", "alert_animation_in": "backInDown", "text_animation": "wiggle", "layout": "banner", "alert_duration": 10, "font_size": 64, "message_template": "", "active": true }, "general": { "layout": "banner", "alert_parries": true, "parry_alert_delay": 3, "approved_manually": false, "censor_timeout": 0, "background_color": "#80ffac", "alert_delay": 3, "censor_recent_events": true }, "comment": {}, "follow": { "alert_duration": 10, "alert_animation_in": "backInDown", "message_template": "", "alert_animation_out": "backOutDown", "alert_text_delay": 0, "font_size": 64, "image_url": "https://isetup.vn/tiktok/assets/gif/jumpy-t-rex.gif", "text_highlight_color": "#32c3a6", "text_color": "#ffffff", "layout": "banner", "sound_volume": 50, "text_animation": "wiggle", "font_weight": 800, "sound_url": "https://isetup.vn/tiktok/assets/sound/new-message-4.ogg", "active": true } }`));
   const [layer, setLayer] = useState("log");
@@ -22,7 +23,7 @@ const Alertbox = () => {
   const [eventQueue, setEventQueue] = useState([]);
   const [mainEvent, setMainEvent] = useState({"type": "like", "data" : {}});
   const [animate, setAnimate] = useState("");
-  const [isDelay, setIsDelay] = useState(false);
+  // const [isDelay, setIsDelay] = useState(false);
   const [isShowing, setIsShowing] = useState(false);
   
   const audio = new Audio("https://isetup.vn/tiktok/assets/sound/new-message-4.ogg");
@@ -143,7 +144,7 @@ const Alertbox = () => {
   }
   
   const MessTemplate = () => {
-    if(!mainEvent.data.id){
+    if(!mainEvent.data.id || isDelay.current){
       return (<></>);
     }
     
@@ -164,7 +165,7 @@ const Alertbox = () => {
     .replace("share", (optsTemplate || '{username} just share livestream!'))
     .replace("follow", (optsTemplate || '{username} is now follower'))
     
-    let output =  template.split(" ").map((text, i) => ( <span key={i}>{
+    return template.split(" ").map((text, i) => ( <span key={i}>{
       text === "{username}" ? animateLetter(mainEvent.data.uniqueId) :
       text === "{nickname}" ? animateLetter(mainEvent.data.nickname || mainEvent.data.uniqueId) :
       text === "{giftname}" ? animateLetter(mainEvent.data.extendedGiftInfo.name) :
@@ -172,9 +173,6 @@ const Alertbox = () => {
       text === "{likecount}" ? animateLetter(mainEvent.data.likeCount) :
       text === "{amount}" ? animateLetter(mainEvent.data.gift.repeat_count * mainEvent.data.extendedGiftInfo.diamond_count) : text
     }{' '}</span>))
-    
-    console.log(output.toString())
-    return output
   }
   
   useEffect(function(){
@@ -202,7 +200,7 @@ const Alertbox = () => {
   useEffect(() => {
     let events = [...eventQueue];
     let event = events.pop();
-    if(!isDelay){
+    if(!isDelay.current){
       var delay = options.general.alert_delay
       if(event){
         options.general.alert_parries && setIsShowing(false);
@@ -231,14 +229,14 @@ const Alertbox = () => {
             }
           }, opt.alert_duration*1000)
           
-          setIsDelay(true);
+          isDelay.current = true;
           setTimeout(() => {
-            setIsDelay(false)
+            isDelay.current = false
           }, delay*1000)
         }
       } 
     }
-  }, [eventQueue, isDelay, isShowing])
+  }, [eventQueue, isDelay.current, isShowing])
  
   return (
     <div className="Alertbox">
