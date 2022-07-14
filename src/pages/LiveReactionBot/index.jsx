939aecc12e84519f5e7df37bd3e1cfd68042dcf1
Glitch (tungsten-slender-ellipse) <none>
2022-07-14 02:50:05 +0000

    🍙⚽️ Checkpoint ./src/pages/LiveReactionBot/index.jsx:663034/16919

--- src/pages/LiveReactionBot/index.jsx
+++ src/pages/LiveReactionBot/index.jsx
@@ -266,11 +266,129 @@ const App = () => {
     }
   }, [eventQueue, isDelay, canPlaySound, likeCount, shareCount])
  
+  const handleOptionsChange = async e => {
+    let target = e.target;
+    let type = target.type;
+    let optType = target.closest("form").name;
+    let optName = target.name;
+    var value = target.value;
+    
+    switch(type){
+      case 'number':
+        value = parseInt(value.replace('e', '') || options[optType][optName]);
+        target.value = value;
+        break;
+      case 'range':
+        value = parseInt(value);
+        break;
+      case 'radio':
+        (value === "true" || value === "false") && (value = (value === 'true'));
+        break;
+      default:
+        break;
+    }
+    
+    switch(optName){
+      case "text_color":
+        if(!(/^#[0-9a-f]{3}([0-9a-f]{3})?$/i.test(value))){
+          value = "#ffffff";
+        } 
+        break;
+      case "text_highlight_color":
+        if(!(/^#[0-9a-f]{3}([0-9a-f]{3})?$/i.test(value))){
+          value = "#ffffff";
+        } 
+        break;
+      default:
+        break;
+    }
+    
+    options[optType][optName] = value;
+    setOptions(options);
+  }
+  
   function SettingLayer(){
-    Object.keys(options).map(function(type, i){
-      if(type ==)
-    })
-    return (<span>abc</span>)
+    return (<div className="card w-100 h-100">
+    <div className="card-header">
+      <ul className="nav nav-tabs" id="pills-tab" role="tablist">
+        <li className="nav-item" role="presentation">
+          <button className="nav-link fs-6 py-2 px-3 active" data-mdb-toggle="tab" href="#tab-general" role="tab" aria-controls="tab-general" aria-selected="true">General</button>
+        </li>
+        <li className="nav-item" role="presentation">
+          <button className="nav-link fs-6 py-2 px-3" data-mdb-toggle="tab" href="#tab-gift" role="tab" aria-controls="tab-gift" aria-selected="false">Donate</button>
+        </li>
+        <li className="nav-item" role="presentation">
+          <button className="nav-link fs-6 py-2 px-3" data-mdb-toggle="tab" href="#tab-follow" role="tab" aria-controls="tab-follow" aria-selected="false">Follow</button>
+        </li>
+        <li className="nav-item" role="presentation">
+          <button className="nav-link fs-6 py-2 px-3" data-mdb-toggle="tab" href="#tab-like" role="tab" aria-controls="tab-like" aria-selected="false">Like</button>
+        </li>
+        <li className="nav-item" role="presentation">
+          <button className="nav-link fs-6 py-2 px-3" data-mdb-toggle="tab" href="#tab-share" role="tab" aria-controls="tab-share" aria-selected="false">Share</button>
+        </li>
+        <li className="ms-auto" role="presentation">
+          <button onClick={e => {setLayer("play")}} className="btn btn-lg btn-light position-fixed top-0 end-0 text-primary lh-1 p-2 m-3" style={{"zIndex":"1"}}><i className="fas fa-times-circle"></i></button>
+        </li>
+      </ul>
+    </div>
+    <div className="card-body text-start overflow-auto">
+      <div className="tab-content pt-3" id="pills-tabContent">
+        <div className="tab-pane fade show active" id="tab-general" role="tabpanel" aria-labelledby="general">
+          <form name="general">
+            <div className="row mb-3">
+              <div className="col-4">
+                <label className="form-label mb-0">Alert delay</label>
+              </div>
+              <div className="col-8">
+                <div className="range">
+                    <input name="alert_delay" defaultValue={options.general.alert_delay} onChange={handleOptionsChange} type="range" className="form-range" min="0" max="30" step="1" />
+                </div>
+                </div>
+            </div>
+            <div className="row mb-3">
+              <div className="col-4">
+                <label className="form-label mb-0">Parry alert</label>
+              </div>
+              <div className="col-8">
+                <div className="form-check d-inline-block">
+                  <input name="alert_parries" defaultValue="true" onChange={handleOptionsChange} className="form-check-input" type="radio" defaultChecked={options.general.alert_parries}/>
+                  <label className="form-check-label">
+                    On
+                  </label>
+                </div>
+                <div className="form-check d-inline-block ms-3">
+                  <input name="alert_parries" defaultValue="false" onChange={handleOptionsChange} className="form-check-input" type="radio" defaultChecked={!options.general.alert_parries}/>
+                  <label className="form-check-label">
+                    Off
+                  </label>
+                </div>
+              </div>
+            </div>
+            <div className="row mb-3 parry_alert_delay" style={{"display": (options.general.alert_parries ? "flex" : "none")}}>
+              <div className="col-4">
+                <label className="form-label mb-0">Parry delay</label>
+              </div>
+              <div className="col-8">
+                <div className="range">
+                  <input name="parry_alert_delay" defaultValue={options.general.parry_alert_delay} onChange={handleOptionsChange} type="range" className="form-range" min="1" max="20" step="1" data-before="1" data-before-subfix="s"/>
+                </div>
+                </div>
+            </div>
+          </form>
+        </div>
+        {Object.keys(options).map(function(type, i){
+          if(type !== "general"){ return (
+            <div key={type} className="tab-pane fade" id={`tab-${type}`} role="tabpanel">
+              <form name={type}>
+                
+              </form>
+            </div>
+          ) }
+        })}
+        </div>
+      </div>
+    </div>
+    )
   }
   
   return (
